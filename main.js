756a013564898a21ab74082bffeea774f60d7a8b
Roni Laukkarinen <roni@laukkarinen.info>
2017-12-12 13:36:12 +0200

    Add debug features

--- main.js
+++ main.js
@@ -7,6 +7,19 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
+// Debug settings
+require('electron-debug')({
+  showDevTools: true,
+  enabled: true
+});
+
+// Enable inspect element context menu
+require('electron-context-menu')({
+  showInspectElement: true
+});
+
+let win;
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
